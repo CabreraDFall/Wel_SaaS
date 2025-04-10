0eb6038128ba18fa9ca72ba0d59c2fbb8755acd9
CabreraDfall <cabreradfall@gmail.com>
2025-04-10 13:13:16 +0200

    Fixing merge

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
 
 
 // https://vite.dev/config/
@@ -9,10 +9,6 @@ export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
-<<<<<<< HEAD
    
-=======
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
->>>>>>> 12a4bf48120bbb42fa7967eeaa0046bfb9a93987
   ],
 })
