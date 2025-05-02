473cb1f1cd963589b10aa71faa69755ef6ab0369
CabreraDfall <cabreradfall@gmail.com>
2025-05-02 08:47:04 +0200

    Creade Single label

--- frontend/src/pages/dashboard/dashboard.jsx
+++ frontend/src/pages/dashboard/dashboard.jsx
@@ -2,6 +2,7 @@ import './dashboard.css'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navBar/Navbar'
 import React, { useRef } from 'react'; // Status: Se importó useRef
+import LabelCard from '../../utils/print/labelCard';
 
 const Dashboard = () => {
     const componentRef = useRef(); // Status: Se creó una referencia al componente
@@ -21,6 +22,8 @@ const Dashboard = () => {
                 <Navbar />
                 <div className='dashboard-container'>
                     <h1>Dashboard</h1>
+
+                    <LabelCard companyName={"Shellfish del Caribe"} productName={"Costillas de Pollo"} weight={"21.5"} udmCode={"lbs"} barcode={"01-123456789012"} />
                     
                 </div>
             </div>
