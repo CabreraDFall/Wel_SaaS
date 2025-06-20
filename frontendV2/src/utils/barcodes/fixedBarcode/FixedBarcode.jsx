1a5dcb6ba4e8491de3a30eafc0530da73925b64f
CabreraDfall <cabreradfall@gmail.com>
2025-06-20 18:11:36 +0200

    feat: Implement fixed barcode component with basic layout
    This commit introduces the `FixedBarcode` component, which displays a fixed barcode interface. The component includes a top menu, a card displaying product information (name, warehouse, quantity), and buttons for creating and canceling.  A basic layout and styling have been added to the component.

--- frontendV2/src/utils/barcodes/fixedBarcode/FixedBarcode.jsx
+++ frontendV2/src/utils/barcodes/fixedBarcode/FixedBarcode.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import TopMenu from '../../../components/topmenu/TopMenu'
-
+import "./fixed-barcode.css"
+import Btn from '../../../components/button/Btn'
 function FixedBarcode() {
     return (
-        <div><TopMenu title={"Barcode"} /></div>
+        <div className='fixedBarcode'>
+            <TopMenu title={"Barcode"} />
+            <div className="fixedBarcode__content">
+                <div className='fixedBarcode__card'>
+
+                    <div className="fixedBarcode__header">
+                        <h4>producto name</h4>
+                    </div>
+                    <div className="fixedBarcode__body">
+
+                        <h4>almacen</h4>
+                        <h4>cantidad</h4>
+                    </div>
+                    <div className="fixedBarcode__footer">
+                        <Btn text={"Crear"} />
+                        <span>cancelar</span>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
