5b5a0a088fb84cd5080fe11ca1a0d025fbb8a450
CabreraDfall <cabreradfall@gmail.com>
2025-06-23 13:47:48 +0200

    Fix: Correct CSS import path in NewLabel component
    The CSS import path in the NewLabel component was incorrect. This commit fixes the path to ensure that the CSS styles are correctly applied to the component.

--- frontend/src/pages/labels/newLabel/NewLabel.jsx
+++ frontend/src/pages/labels/newLabel/NewLabel.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Layout from '../../../components/Layout'
-import './NewLabel.css'
+import './newLabel.css'
 import EmptyPackage from '../../../components/icons/empty_package'
 import SearchIcon from '../../../components/icons/SearchIcon'
 import { productService } from '../../../services/api/productService'
